a4c8e91a6d9f54602b1161408772186f0c2b124b
Anand Choudhary <30415188+anandpc@users.noreply.github.com>
2017-08-19 13:00:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,8 +10,13 @@ app.get('/', function (req, res) {
 });
 
 app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'page-1.html'));
+});
+
+app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));

464a7fbb36d39799f6acb1c903a4aed4dc77e18b
Anand Choudhary <30415188+anandpc@users.noreply.github.com>
2017-09-03 14:49:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,13 +2,13 @@ var img = document.getElementById('madi');
 var marginLeft = 0 ;
 
 function move(){
-    if(img.style.marginLeft == 1600 +'px'){
-        marginLeft = -1600;
+    if(img.style.marginLeft == 2000 +'px'){
+        marginLeft = -2000;
     }
   marginLeft = marginLeft + 10;
   img.style.marginLeft = marginLeft + 'px';
 }
 
 img.onclick = function(){
-    var interval = setInterval(move,100);
+    var interval = setInterval(move,50);
 };

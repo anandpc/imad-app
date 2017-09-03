fe0be09e068c8370a81956275020d01a50c192c5
Anand Choudhary <30415188+anandpc@users.noreply.github.com>
2017-09-03 15:16:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -96,7 +96,7 @@ app.get('/:pageName', function (req, res){
 
 // Counter page
 var counter = 0;
-app.get('/counter', function(req, res){
+app.get('/counter', function(req, res) {
     counter = counter + 1;
     res.send(counter.toString());
 });

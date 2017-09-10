82bc7f21abe1c3fe1dd34cbfdc3b64bd1fd15fc5
Anand Choudhary <30415188+anandpc@users.noreply.github.com>
2017-09-10 22:07:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,6 @@
 // Image Animation 
-
 var img = document.getElementById('madi');
 var marginLeft = 0 ;
-
 function move(){
     if(img.style.marginLeft == 2000 +'px'){
         marginLeft = -2000;
@@ -10,18 +8,27 @@ function move(){
   marginLeft = marginLeft + 10;
   img.style.marginLeft = marginLeft + 'px';
 }
-
 img.onclick = function(){
     var interval = setInterval(move,50);
 };
 
 
 // counter Code
-
 var counter = 0;
-var btn = document.getElementById('btn');
-btn.onclick = function(){
+function load_counter() {
+    // Creating request Object.
+    var request = new XMLHttpRequest();
+    
+    // request state change
+    
+    request.onreadystatechange = function() {
+        if(request.status == 200)
+        
+    };
+    
+    
+    
     counter = counter + 1;
     var span = document.getElementById('count');
     span.innerHTML = counter.toString();
-};
+}

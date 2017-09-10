dcdc04b3889e0bd29255e83f4a97a3394775f325
Anand Choudhary <30415188+anandpc@users.noreply.github.com>
2017-09-10 22:15:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -14,21 +14,23 @@ img.onclick = function(){
 
 
 // counter Code
-var counter = 0;
 function load_counter() {
     // Creating request Object.
     var request = new XMLHttpRequest();
     
     // request state change
-    
-    request.onreadystatechange = function() {
-        if(request.status == 200)
+        request.onreadystatechange = function() {
+            if(request.readyState == XMLHttpRequest.DONE) {
+                if(request.status == 200){
+                    var counter = request.responseText;
+                    var span = document.getElementById('count');
+                     span.innerHTML = counter.toString();
+                }
+            }
+            
+        };
         
-    };
-    
-    
-    
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+        // Make a request
+        request.open('GET', 'http://anandpc13.imad.hasura-app.io/counter',true);
+        request.send(null);
 }

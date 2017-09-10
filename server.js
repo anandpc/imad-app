7a91ef435834583e7aecd8d4652ebbfd2a039c10
Anand Choudhary <30415188+anandpc@users.noreply.github.com>
2017-09-10 22:19:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -86,11 +86,11 @@ function createTemplate (data){
     return htmlTemplate;
 }
 
-var counter = 0;
+/*var counter = 0;
 app.get ('/counter', function(req, res){
     counter += 1;
     res.send(counter.toString());
-});
+});*/
 
 // express framework url mapping
 app.get('/:pageName', function (req, res){

961460fc6cd47bef6e4e5539fc81406ded1df23e
Anand Choudhary <30415188+anandpc@users.noreply.github.com>
2017-09-30 21:47:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -56,11 +56,11 @@ function createTemplate (data){
 }
 
 
-var Pool = new Pool(config);
+var pool = new Pool(config);
 //database
 app.get('/db', function(req, res){
     // make a select request
-    Pool.query('select * from article', function(err, result){
+    pool.query('select * from article', function(err, result){
        if(err) {
            res.status(500).send(err.toString());
        } else{
